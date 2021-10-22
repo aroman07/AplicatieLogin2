b5b82adbccabc337d12c757f1f4ca9029ae91433
Adrian <adrianroman07@gmail.com>
2021-10-22 11:37:36 +0300

    Test

---
+++ src/MainForm.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainForm {
+    private JPanel rootPanel;
+    private JTextField txtUsername;
+    private JTextField txtPassword;
+    private JButton btnLogin;
+
+    public MainForm() {
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int result = JOptionPane.showConfirmDialog(null,
+                        "Are you sure you want to log in?",
+                        "Question",
+                        JOptionPane.YES_NO_CANCEL_OPTION,
+                        JOptionPane.QUESTION_MESSAGE);
+
+                if (result == JOptionPane.YES_OPTION){
+                    System.out.println("Yes");
+                }
+                else
+                    System.out.println("No");
+
+                System.out.println(result);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new MainForm().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
